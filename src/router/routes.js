e98d06d7b2d0bc7363235bf1868e310902ac8f47
KhomeshG <107702111+KhomeshG@users.noreply.github.com>
2023-02-03 18:23:17 +0530

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,20 +6,48 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-const s3Bucket = require("../controller/awsS3");
-
-const { uploadFile } = require("../awsCred");
-
 router.get("/test-me", (req, res) => {
   res.send({ msg: "Hi" });
 });
 
-router.post("/photo1/upload", upload.single("image"), s3Bucket.upload);
+// //Uploading images------------>
+// //
+const uploadData = require("../controller/awsbucket/uploadData");
+router.post("/photo/upload", upload.single("image"), uploadData.uploadData);
 
-//Not Recommanded But Working
-router.post("/photo/upload", upload.single("image"), async (req, res) => {
-  await uploadFile(req.file);
-  res.send({ msh: "Sucees" });
-});
+//Get Images from bucket--------->
+const getDataFromBucket = require("../controller/awsbucket/getdata");
+router.get("/photo", upload.single("image"), getDataFromBucket.getData);
+
+//Get all the Imagefiles-------->
+router.get("/photoAll", upload.single("image"), getDataFromBucket.getAllData);
+
+//delete Images----->
+router.delete("/photo", upload.single("image"), getDataFromBucket.deleteData);
+
+//Rabbitmq route--->
+
+// const publishToQueue = require("../controller/rabbitmq");
+// const consummerToQueue = require("../controller/rabbitmq");
+
+//Sending msg to rabbitmq-------->
+
+// router.post("/msg", async (req, res, next) => {
+//   let { queueName, payload } = req.body;
+//   await publishToQueue.publishToQueue(queueName, payload);
+//   res.statusCode = 200;
+//   // res.data = { "message-sent": true };
+//   res.send({ msg: true });
+//   next();
+// });
+
+//Consummeing Data---->
+// /Getingmsg from Rabbitmq-->
+
+// router.get("/msg1", async (req, res, next) => {
+//   await consummerToQueue.consummerToQueue();
+//   res.send({ msg: true });
+//   next();
+// });
 
 module.exports = router;
